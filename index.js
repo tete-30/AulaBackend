5a00570ac8bb468ebba9ba61a6b597f925014e0a
tete-30 <112039750+tete-30@users.noreply.github.com>
2023-12-11 22:00:53 -0300

    Integração Backend com o Frontend

--- index.js
+++ index.js
@@ -1,50 +1,81 @@
 //configuração inicial
 const express = require('express')
 const app = express()
+const cors = requere('cors')
 
 //configuração de banco
 const mongoose = require('mongoose')
 const Login = require('./model/login')
+const Cadastro = require('./model/cadastro')
+
+app.use(cors())
 
 app.use(
     express.urlencoded({
-        extended:true
+        extended: true
     })
 )
 
 //criação das rotas
 app.use(express.json())
 
-app.post('/login', async(req, res) =>{
-    const {email,senha} = req.body
-    const login = {
-        email,
-        senha
-    }
-
-    try{
-        await Login.create(login)
-        res.status(201).json({Message: 'Fez login'})
-    } catch(console) {
-        res.status(500).json({error:error})
+app.post('/login', async (req, res) => {
+    const { email, senha } = req.body
+   
+    try {
+        const usuario = await Cadastro.findOne({email,senha});
+        if (usuario){
+            res.status(200).json({message: "Login efetuado com suuuuuuuuuucesso!"});
+        } else {
+            res.status(401).json({message: "Erro ao fazer login. Verificar suas credenciais"});
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message})
     }
 })
 
-app.get('/login', async(req,res) => {
-    try{
+app.get('/login', async (req, res) => {
+    try {
         const login = await Login.find()
         res.status(200).json(login)
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({ error: error })
     }
 
 })
 
-mongoose.connect('mongodb+srv://portohortensia1992:160215@teste.gr1cvpa.mongodb.net/?authMechanism=SCRAM-SHA-1')
-.then(()=> {
-console.log('Conectou')
-app.listen(3000)
+app.post('/cadastro', async (req, res) => {
+    const {nome, cpf, enderecog, numero, complemento, cep, bairro, cidade, uf, telefone, celular, email, senha, confirmeSenha} = req.body
+    const login = {
+        nome, 
+        cpf, 
+        enderecog, 
+        numero, 
+        complemento, 
+        cep, 
+        bairro, 
+        cidade, 
+        uf, 
+        telefone, 
+        celular, 
+        email, 
+        senha, 
+        confirmeSenha,
+    }
 
+    try {
+        await cadastro.create(login)
+        res.status(201).json({ Message: 'Cadastro realizado com suuuuuuuuuucesso!' })
+    } catch (console) {
+        res.status(500).json({ error: error })
+    }
 })
 
-.catch((err)=>console.log(err))
+mongoose.connect('mongodb+srv://portohortensia1992:160215@teste.1yb8dry.mongodb.net/')
+    .then(() => {
+        console.log('Conectou')
+        app.listen(3000)
+
+    })
+
+    .catch((err) => console.log(err))
---
+++ model/cadastro.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Cadastro = mongoose.model('cadastro',{
+    nome:String,
+    cpf:String,
+    endereco:String,
+    numero:int,
+    complemento:String,
+    cep:String,
+    bairro:String,
+    cidade:String,
+    uf:String,
+    telefone:String,
+    celular:String,
+    email:String,
+    senha:String,
+    confirmeSenha:String,    
+})
+
+module.exports = Cadastro
